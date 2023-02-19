aa134a08eb092f7675b1755fe59d894d191f8274
PerhapsChen <451622433@qq.com>
2023-02-19 16:25:15 +0800

    random in unit sphere

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,23 @@
 #include "sphere.h"
 #include "camera.h"
 
-color ray_color(const ray& r, const hittable& world, int depth) {
+color ray_color(const ray& r, const hittable& world, int depth){
     hit_record rec;
 
-    // If we've exceeded the ray bounce limit, no more light is gathered.
-    if (depth <= 0)
+    // too much bounce, no energy to eye.
+    if(depth <= 0){
         return color(0,0,0);
-
-    if (world.hit(r, 0, infinity, rec)) {
-        point3 target = rec.p + rec.normal + random_in_unit_sphere();
-        return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);
     }
 
+    if(world.hit(r,0.001,infinity,rec)){ //! recur
+        //point3 target = rec.p + rec.normal + random_in_unit_sphere();
+        point3 target = rec.p + rec.normal + random_unit_vector();
+        return 0.5 * ray_color(ray(rec.p, target-rec.p), world, depth-1);
+    }
+    // else -> still background.
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+    return (1.0-t) * color(1.0,1.0,1.0) + t*color(0.5,0.7,1.0);
 }
 
 
