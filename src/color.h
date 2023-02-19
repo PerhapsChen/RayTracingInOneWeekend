aa6a79872e72f533b3af5c87b6a446e0fb93fe21
PerhapsChen <451622433@qq.com>
2023-02-19 16:23:33 +0800

    my imp

--- src/color.h
+++ src/color.h
@@ -12,10 +12,10 @@ void write_color(std::ofstream &fout, color pixel_color, int samples_per_pixel)
     auto b = pixel_color.z();
 
     // Divide the color by the number of samples.
-    auto Scale = 1.0 / samples_per_pixel;
-    r *= Scale;
-    g *= Scale;
-    b *= Scale;
+    auto scale = 1.0 / samples_per_pixel;
+    r = sqrt(scale * r);
+    g = sqrt(scale * g);
+    b = sqrt(scale * b);
 
     // Write the translated [0,255] value of each color component.
     fout << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
