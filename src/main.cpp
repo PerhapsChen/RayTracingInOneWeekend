ac2420e44c81833427b06fa9c4f4e65e00d4c3f9
PerhapsChen <451622433@qq.com>
2023-02-19 11:08:29 +0800

    vec3 class added.

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,33 @@
 #include <iostream>
 #include <fstream>
+#include "vec3.h"
 
-inline float clamp(const float &lo, const float &hi, const float &v)
-{ return std::max(lo, std::min(hi, v)); }
-
+void write_color(std::ofstream &fout, color pixel_color) {
+    // Write the translated [0,255] value of each color component.
+    fout << static_cast<int>(255.999 * pixel_color.x()) << ' '
+         << static_cast<int>(255.999 * pixel_color.y()) << ' '
+         << static_cast<int>(255.999 * pixel_color.z()) << ' ';
+}
 
 int main() {
 
     // Image
-
     const int image_width = 256;
     const int image_height = 256;
 
-    // Render
-
-
-
     // save framebuffer to file
-    const std::string outputFile {"C:/Users/45162.CPHXR9000K/Desktop/RayTracing/output/fig.ppm"};
+    const std::string outputFile {"C:/Users/45162.CPHXR9000K/Desktop/RayTracing/output/fig2.ppm"};
     std::ofstream fout(outputFile);
     fout << "P3\n" << image_width << " " << image_height <<"\n255\n";
     for (int j = image_height-1; j >= 0; --j) {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
-            auto b = 0.25;
-
-            auto ir = static_cast<int>(255.999 * r);
-            auto ig = static_cast<int>(255.999 * g);
-            auto ib = static_cast<int>(255.999 * b);
-
-            fout << ir << ' ' << ig << ' ' << ib << ' ';
+            color pixel_color(double(i)/(image_width-1),
+                              double(j)/(image_height-1), 0.25);
+            write_color(fout, pixel_color);
         }
     }
     fout.close();
+
+    std::cerr << "\nDone.\n";
 }
