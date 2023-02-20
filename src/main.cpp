3558c413e1aef785d0bd5a4fb63d6fe78e166dbe
PerhapsChen <451622433@qq.com>
2023-02-20 22:22:38 +0800

    image texture (earth)

--- src/main.cpp
+++ src/main.cpp
@@ -82,6 +82,15 @@ hittable_list two_perlin_spheres()
     return objects;
 }
 
+hittable_list earth()
+{
+    auto earth_texture = make_shared<image_texture>("/home/chen-p/perhaps/RayTracing/assets/earthmap.jpg");
+    auto earth_surface = make_shared<lambertian>(earth_texture);
+    auto globe = make_shared<sphere>(point3(0,0,0), 2, earth_surface);
+
+    return hittable_list(globe);
+}
+
 color ray_color(const ray& r, const hittable& world, int depth){
     hit_record rec;
 
@@ -166,13 +175,20 @@ int main() {
             vfov = 20.0;
             break;
 
-        default:
         case 3:
             world = two_perlin_spheres();
             lookfrom = point3(13,2,3);
             lookat = point3(0,0,0);
             vfov = 20.0;
             break;
+
+        default:
+        case 4:
+            world = earth();
+            lookfrom = point3(13,2,3);
+            lookat = point3(0,0,0);
+            vfov = 20.0;
+            break;        
     }
 
     //Camera
