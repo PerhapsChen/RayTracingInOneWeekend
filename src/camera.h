0ff26515e11d7e8c16ccadc147563419de7cdd5d
PerhapsChen <451622433@qq.com>
2023-02-20 13:29:40 +0800

    final fig

--- src/camera.h
+++ src/camera.h
@@ -5,28 +5,39 @@
 class camera
 {
 public:
-    camera(double vfov, double aspect_ratio){
+    camera(point3 lookfrom, point3 lookat, vec3 vup, double vfov, double aspect_ratio, double aperture, double focus_dist){
         auto theta = degrees_to_radians(vfov);
         auto h = tan(theta/2);
         auto viewport_height = 2.0 * h;
         auto viewport_width = aspect_ratio * viewport_height;   
 
-        double focal_lenght = 1.0; //焦距
+        w = unit_vector(lookfrom - lookat);
+        u = unit_vector(cross(vup, w));
+        v = cross(w, u);
 
-        origin = point3(0.0,0.0,0.0);
+        origin = lookfrom;
+        horizontal = focus_dist * viewport_width * u;
+        vertical = focus_dist * viewport_height * v;
+        lower_left_corner = origin - horizontal/2 - vertical/2 - focus_dist*w;
 
-        horizontal = vec3(viewport_width, 0, 0);
-        vertical = vec3(0, viewport_height, 0);
-        lower_left_corner = 
-            point3(origin - horizontal/2 - vertical/2 - vec3(0,0,focal_lenght));
+        lens_radius = aperture / 2;
     }
-    ray get_ray(double u, double v) const{
-        return ray(origin, lower_left_corner+u*horizontal+v*vertical - origin);
+    ray get_ray(double s, double t) const{
+        vec3 rd = lens_radius * random_in_unit_disk();
+        vec3 offset = u * rd.x() + v * rd.y();
+
+        return ray(
+            origin + offset,
+            lower_left_corner + s*horizontal + t*vertical - origin - offset
+        );
     }
 private:
     point3 origin;
     point3 lower_left_corner;
     vec3 horizontal;
     vec3 vertical;
+
+    vec3 u,v,w;
+    double lens_radius;
 
 };
