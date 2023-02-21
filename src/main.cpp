e205c2b45562ae3d6227023263ddf806a3c33833
PerhapsChen <451622433@qq.com>
2023-02-21 14:33:39 +0800

    rotate and translate

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #include "material.h"
 #include "moving_sphere.h"
 #include "aarect.h"
+#include "box.h"
 
 hittable_list random_scene() {
     hittable_list world;
@@ -105,6 +106,35 @@ hittable_list simple_light()
     return objects;
 }
 
+hittable_list cornell_box() 
+{
+    hittable_list objects;
+
+    auto red   = make_shared<lambertian>(color(.65, .05, .05));
+    auto white = make_shared<lambertian>(color(.73, .73, .73));
+    auto green = make_shared<lambertian>(color(.12, .45, .15));
+    auto light = make_shared<diffuse_light>(color(15, 15, 15));
+
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 555, green));
+    objects.add(make_shared<yz_rect>(0, 555, 0, 555, 0, red));
+    objects.add(make_shared<xz_rect>(213, 343, 227, 332, 554, light));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
+    objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
+
+    shared_ptr<hittable> box1 = make_shared<box>(point3(0,0,0), point3(165, 330, 165), white);
+    box1 = make_shared<rotate_y>(box1, 15);
+    box1 = make_shared<translate>(box1, vec3(265,0,295));
+    objects.add(box1);
+
+    shared_ptr<hittable> box2 = make_shared<box>(point3(0,0,0), point3(165, 165, 165), white);
+    box2 = make_shared<rotate_y>(box2, -18);
+    box2 = make_shared<translate>(box2, vec3(130,0,65));
+    objects.add(box2);
+
+    return objects;
+}
+
 color ray_color(const ray& r, const color& background, const hittable& world, int depth){
     hit_record rec;
 
@@ -162,10 +192,10 @@ void write_many_rows(int tIdx,
 
 int main() {
 
-    const auto aspect_ratio = 3.0 / 2.0;
-    const int image_width = 600;
+    auto aspect_ratio = 3.0 / 2.0;
+    int image_width = 600;
     int samples_per_pixel = 50 ;
-    const int max_depth = 50;
+    int max_depth = 50;
 
     //World
 
@@ -176,7 +206,7 @@ int main() {
     auto aperture = 0.0;  
     color background(0,0,0);
 
-    switch (0) {
+    switch (6) {
         case 1:
             world = random_scene();
             background = color(0.7, 0.8, 1.0);
@@ -210,7 +240,7 @@ int main() {
             vfov = 20.0;
             break;      
 
-        default:
+
         case 5:
             world = simple_light();
             samples_per_pixel = 400;
@@ -218,6 +248,21 @@ int main() {
             lookfrom = point3(26,3,6);
             lookat = point3(0,2,0);
             vfov = 20.0;
+            break;
+
+        case 6:
+            world = cornell_box();
+            aspect_ratio = 1.0;
+            image_width = 300;
+            samples_per_pixel = 200;
+            background = color(0,0,0);
+            lookfrom = point3(278, 278, -800);
+            lookat = point3(278,278,0);
+            vfov = 40.0;
+            break;
+
+
+        default:
             break;
     }
 
@@ -230,7 +275,7 @@ int main() {
 
     std::map<int, std::vector<int>> result {}; //- store multi thread result 
     std::vector<std::thread> thread_vec {};
-    int NofT = 10;
+    int NofT = 30;
 
     for(int t = 0; t < NofT; t++)
     {
